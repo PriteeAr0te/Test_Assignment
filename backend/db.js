668789cd46d609ff00aa84d18dc5ac3d7e19c53a
PriteeAr0te <arotepritee111@gmail.com>
2024-02-10 17:15:49 +0530

    Updated database

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
-const mongoURL = "mongodb://localhost:27017/Assignment"
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connectToMongo = async() => {
-    try{
-        await mongoose.connect(mongoURL)
-        console.log("Connected to MongoDB Successfully")
-    }
-    catch(error){
-        console.log("Error Occurred during Connecting to database")
-    }
-}
+const mongoURL = process.env.MONGO_URL;
+
+const connectToMongo = async () => {
+  try {
+   const client= await mongoose.connect(mongoURL);
+    console.log("Connected to MongoDB Successfully");
+  } catch (error) {
+    console.log("Error Occurred during Connecting to database");
+  }
+};
 
 module.exports = connectToMongo;
--- frontend/src/Context/Data/dataState.js
+++ frontend/src/Context/Data/dataState.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import dataContext from "./dataContext";
 
 const useDataState = () => {
-  const BASE_URL = "https://datavisualization-backend1852.onrender.com";
+  const BASE_URL = "http://localhost:5000";
   const dataInitial = [];
   const [data, setData] = useState(dataInitial);
 
